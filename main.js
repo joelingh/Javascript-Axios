f77ecdd185545caf7f412f9ff1feb9109faef01f
joelingh <89427426+joelingh@users.noreply.github.com>
2021-09-08 09:27:34 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+const URL = 'https://jsonplaceholder.typicode.com/'
+
+// -- Import Axios 
+import Axios from 'axios'
+
+// Make a request for a user with a given ID
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+
+// Optionally the request above could also be done as
+axios.get('/user', {
+    params: {
+      ID: 12345
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  }); 
+
+
+//   Creating an instance
+//   You can create a new instance of axios with a custom config.
+  
+axios.create([config])
+const instance = axios.create({
+    baseURL: 'https://some-domain.com/api/',
+    timeout: 1000,
+    headers: {'X-Custom-Header': 'foobar'}
+});
+
+// Want to use async/await? Add the `async` keyword to your outer function/method.
+async function getUser() {
+    try {
+      const response = await axios.get('/user?ID=12345');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+//   Performing a POST request
+axios.post('/user', {
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+});
+
+// Requests can be made by passing the relevant config to axios.
+axios(config)
+// Send a POST request
+axios({
+  method: 'post',
+  url: '/user/12345',
+  data: {
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+  }
+});
+
+// Performing multiple concurrent requests
+function getUserAccount() {
+  return axios.get('/user/12345');
+}
+
+function getUserPermissions() {
+  return axios.get('/user/12345/permissions');
+}
+
+Promise.all([getUserAccount(), getUserPermissions()])
+  .then(function (results) {
+    const acct = results[0];
+    const perm = results[1];
+  });
